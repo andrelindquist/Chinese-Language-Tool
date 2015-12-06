f6bdbf7722595731228bff684e80874ceea3d541
Andre_Linquist <andrelindquist@gmail.com>
2015-12-06 02:24:10 -0800

    Fixed typo in config.js file 'database' property

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://andrelindquist:laurent4>@ds061984.mongolab.com:61984/heroku_9szg3kc1', //local dev: 'mongodb://localhost:27017/charapp'
+	'database': 'mongodb://andrelindquist:laurent4@ds061984.mongolab.com:61984/heroku_9szg3kc1', //local dev: 'mongodb://localhost:27017/charapp'
 	'secret': 'noodles'
 };
