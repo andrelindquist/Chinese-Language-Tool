42a6dabbf4f3350b7a30a17642f7d854decb2a9c
Andre_Linquist <andrelindquist@gmail.com>
2016-08-22 15:32:04 -0700

    Add sorting to user's vocab list in dashboard view

--- public/app/controllers/dashboardCtrl.js
+++ public/app/controllers/dashboardCtrl.js
@@ -3,6 +3,8 @@ angular.module('dashboardCtrl', ['userService'])
 .controller('dashboardController', function(User, Auth) {
 
 	var vm = this;
+	vm.sortCriteria = 'proficiency';
+	vm.ascending = true;
 
 	Auth.getUser()
 		.then(function(data) {
@@ -15,6 +17,23 @@ angular.module('dashboardCtrl', ['userService'])
 
 			  	//bind the users that come back to vm.characters
 			  	vm.user = data;
+			  	
 			  });
 		});
+
+	vm.toggleAscending = function() {
+		vm.ascending = !vm.ascending;
+	}
+
+	vm.sort = function(sortCriteria, ascending) {
+		console.log(vm.sortCriteria + "  " + ascending);
+		vm.user.vocabList = vm.user.vocabList.sort(function(a, b) {
+			if (ascending) {
+				return a[sortCriteria] - b[sortCriteria];	
+			}
+			else {
+				return b[sortCriteria] - a[sortCriteria];
+			}
+		});
+	}
 })
